906bb795a08b669da1f6e7653635abc4e87e4d8f
meraymond2 <michael.raymond@mobillity.co>
2017-03-06 14:50:32 +0000

    simplify bool

--- dev/verificationService.js
+++ dev/verificationService.js
@@ -6,17 +6,15 @@ export default function verify(kS: KeyStroke[]): Promise<boolean> {
 
 	// If the length is correct,
 	const inputIsValid: boolean = (kS.length == 4) ? (
-		(
-			// and the first is 'a' and short,
-			(kS[0].keyCode == 65) && (isShort(kS[0].duration)) &&
-			// and the second is 'b' and short, and just after the first,
-			(kS[1].keyCode == 66) && (isShort(kS[1].duration)) && (kS[1].start - kS[0].end < 200) &&
-			// and the third is 'c', and long, and just after the second,
-			(kS[2].keyCode == 67) && (isLong(kS[2].duration)) && (kS[2].start - kS[1].end < 200) &&
-			// and the fourth is 'd', and short, and at over a second after the third.
-			(kS[3].keyCode == 68) && (isShort(kS[3].duration)) && (kS[3].start - kS[2].end > 1000)
-			// then it's valid!
-		) ? true : false	
+		// and the first is 'a' and short,
+		(kS[0].keyCode == 65) && (isShort(kS[0].duration)) &&
+		// and the second is 'b' and short, and just after the first,
+		(kS[1].keyCode == 66) && (isShort(kS[1].duration)) && (kS[1].start - kS[0].end < 200) &&
+		// and the third is 'c', and long, and just after the second,
+		(kS[2].keyCode == 67) && (isLong(kS[2].duration)) && (kS[2].start - kS[1].end < 200) &&
+		// and the fourth is 'd', and short, and at over a second after the third.
+		(kS[3].keyCode == 68) && (isShort(kS[3].duration)) && (kS[3].start - kS[2].end > 1000)
+		// then it's valid!
 	) : false;
 
 	// returns promise because it's mocking a server call
